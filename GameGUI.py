07ce2cba0f0018192053eabd7704e2d6cec1355d
opethe1st <ogunks@live.com>
2015-10-04 18:58:47 +0100

    Working windows version

--- GameGUI.py
+++ GameGUI.py
@@ -6,98 +6,183 @@ import wx
 import time as t
 
 class ArithmeticGame(wx.Frame):
+    """This is the main frame """
     def __init__(self,parent):
-        wx.Frame.__init__(self,parent,pos=(400,300),title = "Arithmetic Game")
+        wx.Frame.__init__(self,parent,pos=(400,300),title = "'rithmetik")
+        self.times = 5
+        self.NUMROUNDS = self.times
+        self.numCorrectAns = 0
+        self.CorrectAns = None
+        self.RunBefore = False
+        #question = None
+        #self.inputAns = None
+
+        self.size = (350,200)
+        self.nest = 1
+        self.answer = None
         self.run()
 
     def startPage(self,panel):
-        panel = panel
-        MainSizer = wx.BoxSizer(wx.VERTICAL)
-        self.sizer = wx.BoxSizer(wx.VERTICAL)
+        """This the start page"""
+        FrameSizer = wx.BoxSizer(wx.VERTICAL)
+        panelSizer = wx.BoxSizer(wx.VERTICAL)
 
-        label = wx.StaticText(panel,label="\tShall\n\tWe \n\tBegin?\n")
-        self.sizer.Add(label)
+        label = wx.StaticText(panel,label="Shall We Begin?")
+        font = wx.Font(17,wx.FONTFAMILY_DEFAULT,wx.FONTSTYLE_NORMAL,wx.FONTWEIGHT_BOLD)
+        label.SetFont(font)
 
         startStop = wx.Panel(panel)
         ynsizer = wx.BoxSizer(wx.HORIZONTAL)
         yes = wx.Button(startStop,label="yes")
-        self.Bind(wx.EVT_BUTTON,self.onYes,yes)
+        self.Bind(wx.EVT_BUTTON,self.newQ,yes)
         no = wx.Button(startStop,label= "no")
-        #self.Bind(wx.EVT_BUTTON,exit(0),no)
+        self.Bind(wx.EVT_BUTTON,self.resultPage,no)
         ynsizer.Add(yes)
         ynsizer.Add(no)
         startStop.SetSizer(ynsizer)
+        
+        panelSizer.AddStretchSpacer(1)
+        panelSizer.Add(label,0,wx.ALIGN_CENTER)
+        panelSizer.Add(startStop,0,wx.ALIGN_CENTER)
+        panelSizer.AddStretchSpacer(1)
 
-        self.sizer.Add(startStop)
+        panel.SetSizer(panelSizer)
 
-        panel.SetSizer(self.sizer)
-        MainSizer.Add(panel)
-        self.SetSizer(MainSizer)
-        self.SetSize((300,100))
+        FrameSizer.Add(panel)
+
+        self.SetSizerAndFit(FrameSizer)
         self.Show()
 
-    def newQuestion(self,panel,numOfTimes):
-        """A method to display Questions"""
-        if numOfTimes>=4:
-            return
-        MainSizer = wx.BoxSizer(wx.VERTICAL)
-        self.sizer = wx.BoxSizer(wx.VERTICAL)
+    def newQuestion(self,panel):
+        """A method to display the Questions page"""
+        self.Layout()
+        FrameSizer = wx.BoxSizer(wx.VERTICAL)
+        panelSizer = wx.BoxSizer(wx.VERTICAL)
 
-        panel = panel
-        expr,self.CorrectAns = GameLogic.generateQuestion(1,1)
-        self.question = wx.StaticText(panel,label="Solve\n %s"%expr)
-        self.sizer.Add(self.question)
+        expr,self.CorrectAns = GameLogic.generateQuestion(1,self.nest)
 
-        self.inputAns = wx.TextCtrl(panel,style = wx.TE_PROCESS_ENTER)
-        self.sizer.Add(self.inputAns)
+        question = wx.StaticText(panel,label="%s = \n"%expr)
+        font = wx.Font(15,wx.FONTFAMILY_DEFAULT,wx.FONTSTYLE_NORMAL,wx.FONTWEIGHT_NORMAL)
+        question.SetFont(font)
+
+        #inp = wx.Panel(panel)
+        self.inputAns = wx.TextCtrl(panel,style = wx.TE_PROCESS_ENTER,size=(30,20))
         self.Bind(wx.EVT_TEXT_ENTER,self.correctAns,self.inputAns)
 
-        self.answer = wx.TextCtrl(panel,style = wx.TE_READONLY,size=(150,25) )
-        self.answer.SetValue("Answer Now!")
-        self.sizer.Add(self.answer)
+        panelSizer.AddStretchSpacer(1)
+        panelSizer.Add(question,0,wx.ALIGN_CENTER)
+        panelSizer.Add(self.inputAns,0,wx.ALIGN_CENTER)
+        panelSizer.AddStretchSpacer(1)
 
-        self.next = wx.Button(panel,label="Next")
-        self.sizer.Add(self.next)
-        self.Bind(wx.EVT_BUTTON,self.newQ,self.next)
-
-        panel.SetSizer(self.sizer)
-        MainSizer.Add(panel)
-
-        self.SetSizerAndFit(MainSizer)
-        self.SetSize((300,100))
+        panel.SetSizer(panelSizer)
+        FrameSizer.Add(panel)
+        self.SetSizerAndFit(FrameSizer)
+        self.Layout()
         self.Show()
 
-    def resultPage():
-        pass
-
-    def update(self,e):
-        self.panel.Destroy()
+    def correctPage(self,panel):
         self.Layout()
-        self.panel = wx.Panel(self)
-        self.newQuestion(self.panel,0)
+        FrameSizer = wx.BoxSizer(wx.VERTICAL)
+        panelSizer = wx.BoxSizer(wx.VERTICAL)
+        CorrectMessage = wx.StaticText(panel,label = "Correct!")
+        font = wx.Font(15,wx.FONTFAMILY_DEFAULT,wx.FONTSTYLE_NORMAL,wx.FONTWEIGHT_BOLD)
+        panelSizer.AddStretchSpacer(1)
+        panelSizer.Add(CorrectMessage,0,wx.ALIGN_CENTER)
+        panelSizer.AddStretchSpacer(1)
+        CorrectMessage.SetFont(font)
 
-
-    def onYes(self,e):
-        self.panel.Destroy()
+        panel.SetSizer(panelSizer)
+        FrameSizer.Add(panel)
+        self.SetSizer(FrameSizer)
         self.Layout()
-        self.panel = wx.Panel(self)
-        self.newQuestion(self.panel,0)
+        self.Show()
+
+    def wrongPage(self,panel,ans):
+        self.Layout()
+        FrameSizer = wx.BoxSizer(wx.VERTICAL)
+        panelSizer = wx.BoxSizer(wx.VERTICAL)
+        CorrectMessage = wx.StaticText(panel,label = "Wrong!\nThe correct answer is %s"%ans)
+        font = wx.Font(15,wx.FONTFAMILY_DEFAULT,wx.FONTSTYLE_NORMAL,wx.FONTWEIGHT_BOLD)
+        panelSizer.AddStretchSpacer(1)
+        panelSizer.Add(CorrectMessage,0,wx.ALIGN_CENTER)
+        panelSizer.AddStretchSpacer(1)
+        CorrectMessage.SetFont(font)
+
+        panel.SetSizer(panelSizer)
+        FrameSizer.Add(panel)
+        self.SetSizer(FrameSizer)
+        self.Layout()
+        self.Show()
+
+
+    def resultPage(self,panel):
+        self.Layout()
+        FrameSizer = wx.BoxSizer(wx.VERTICAL)
+        panelSizer = wx.BoxSizer(wx.VERTICAL)
+        Result = wx.StaticText(panel,label="You got %d/%d correct "%(self.numCorrectAns,self.NUMROUNDS))
+        panelSizer.AddStretchSpacer()
+        panelSizer.Add(Result,0,wx.ALIGN_CENTER)
+        panelSizer.AddStretchSpacer()
+        #panel.SetSizerAndFit(panelSizer)
+        panel.SetSizer(panelSizer)
+        FrameSizer.Add(panel,0,wx.ALIGN_CENTER)
+        self.SetSizer(FrameSizer)
+        self.Layout()
+        self.Show()
+
+
+    def newQ(self,e):
+        if not self.RunBefore:
+            self.RunBefore=True
+        self.panel.Destroy()
+
+        self.panel = wx.Panel(self,size=self.size)
+        if self.times>0:
+            self.newQuestion(self.panel)
+            self.times-=1
+        else:
+            self.resultPage(self.panel)
 
     def correctAns(self,e):
         ans = self.inputAns.GetLineText(0)
         if GameLogic.checkAnswer(ans,self.CorrectAns):
-            self.answer.SetValue("The answer is correct")
-        else:
-            self.answer.SetValue("The answer is wrong")
+            #self.answer.SetValue("The answer is correct")
+            self.numCorrectAns+=1
+            self.panel.Destroy()
+            self.panel = wx.Panel(self,size=self.size)
+            self.correctPage(self.panel)
+            self.timer = wx.Timer(self)
+            self.timer.Start(1500)
+            self.Bind(wx.EVT_TIMER,self.update,self.timer)
 
-    def newQ(self,e):
-        self.panel.Destroy()
-        self.Layout()
-        self.panel = wx.Panel(self)
-        self.newQuestion(self.panel,0)
+        else:
+            self.panel.Destroy()
+            self.panel = wx.Panel(self,size=self.size)
+            self.wrongPage(self.panel,self.CorrectAns)
+            self.timer = wx.Timer(self)
+            self.timer.Start(1500)
+            self.Bind(wx.EVT_TIMER,self.update,self.timer)
+            pass
+            #self.answer.SetValue("The answer is wrong. The correct Answer is %s"%self.CorrectAns)
+        #self.next.Enable()
+
+    def update(self,e):
+        if self.timer.IsRunning():
+            self.timer.Stop()
+            self.panel.Destroy()
+            self.panel = wx.Panel(self,size=self.size)
+            self.newQuestion(self.panel)
+            self.times-=1
+
+        if self.times==0:
+            self.panel.Destroy()
+            self.panel = wx.Panel(self,size=self.size)
+            self.resultPage(self.panel)
+
+
 
     def run(self):
-        self.panel = wx.Panel(self)
+        self.panel = wx.Panel(self,size=self.size)
         self.startPage(self.panel)
 
 
